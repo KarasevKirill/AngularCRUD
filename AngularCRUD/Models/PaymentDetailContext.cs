5ffef86f46a2a96102add4d97b03bf7eb0c404d8
Kirill Karasev <karasev.kirill.a@gmail.com>
2019-05-12 21:24:08 +1000

    Add EF

---
+++ AngularCRUD/Models/PaymentDetailContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AngularCRUD.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options): base(options)
+        {
+
+        }
+    }
+}
